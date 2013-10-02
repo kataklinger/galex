010536b32b93801f1ee08cb1c5b348a51f0028c7
kataklinger <kataklinger@gmail.com>
2013-10-02 16:13:25 +0200

    csp - basic placement algorithm

--- Problems/CSP.cpp
+++ Problems/CSP.cpp
@@ -1,10 +1,166 @@
 
 #include "CSP.h"
 
+#include <algorithm>
+
 namespace Problems
 {
 	namespace BPP
 	{
+
+		Point& Point::operator =(const Point& rhs)
+		{
+			_x = rhs._x;
+			_y = rhs._y;
+
+			return *this;
+		}
+
+		Point& Point::operator +=(const Size& rhs)
+		{
+			_x += rhs.GetWidth();
+			_y += rhs.GetHeight();
+
+			return *this;
+		}
+
+		Point& Point::operator -=(const Size& rhs)
+		{
+			_x -= rhs.GetWidth();
+			_y -= rhs.GetHeight();
+
+			return *this;
+		}
+
+		Size& Size::operator +=(const Size& rhs)
+		{
+			_width += rhs._width;
+			_height += rhs._height;
+
+			return *this;
+		}
+
+		Size& Size::operator -=(const Size& rhs)
+		{
+			_width -= rhs._width;
+			_height -= rhs._height;
+
+			return *this;
+		}
+
+		Size& Size::operator =(const Size& rhs)
+		{
+			_width = rhs._width;
+			_height = rhs._height;
+
+			return *this;
+		}
+
+		void AddSlot(std::vector<Slot>& slots, const Slot& slot)
+		{
+			for( std::vector<Slot>::iterator it = slots.begin(); it != slots.end(); ++it )
+			{
+				if( it->Merge( slot ) )
+					return;
+			}
+
+			slots.push_back( slot );
+		}
+
+		void Slot::Place(const Placement& placement, std::vector<Slot>& slots)
+		{
+			if( _area.IsOverlapping( placement.GetArea() ) )
+			{
+				int distance;
+				const Rectangle& space = placement.GetArea();
+
+				distance = space.GetPosition().GetX() - _area.GetPosition().GetX();
+				if( distance > 0 )
+					AddSlot( slots, Slot( _area.GetPosition(), Size( distance, _area.GetSize().GetHeight() ) ) );
+
+				distance = _area.GetLimit().GetX() - space.GetLimit().GetX();
+				if( distance > 0 )
+					AddSlot( slots, Slot( Point( space.GetLimit().GetX(), _area.GetPosition().GetY() ), Size( distance, _area.GetSize().GetHeight() ) ) );
+
+				distance = space.GetPosition().GetY() - _area.GetPosition().GetY();
+				if( distance > 0 )
+					AddSlot( slots, Slot( _area.GetPosition(), Size( _area.GetSize().GetWidth(), distance ) ) );
+
+				distance = _area.GetLimit().GetY() - space.GetLimit().GetY();
+				if( distance > 0 )
+					AddSlot( slots, Slot( Point( _area.GetPosition().GetX(), space.GetLimit().GetY() ), Size( _area.GetSize().GetWidth(), distance ) ) );
+			}
+			else
+				AddSlot( slots, *this );
+		}
+
+		bool Slot::Merge(const Slot& slot)
+		{
+			if( _area.GetSize().GetWidth() == slot._area.GetSize().GetWidth() && _area.GetPosition().GetX() == slot._area.GetPosition().GetX() )
+			{
+				if( ( slot._area.GetPosition().GetY() >= _area.GetPosition().GetY() && slot._area.GetPosition().GetY() <= _area.GetLimit().GetY() ) ||
+					( slot._area.GetLimit().GetY() >= _area.GetPosition().GetY() && slot._area.GetLimit().GetY() <= _area.GetLimit().GetY() ) )
+				{
+					int y1 = std::min( slot._area.GetPosition().GetY(), _area.GetPosition().GetY() );
+					int y2 = std::max( slot._area.GetLimit().GetY(), _area.GetLimit().GetY() );
+
+					_area = Rectangle( Point( slot._area.GetPosition().GetX(), y1 ), Size( _area.GetSize().GetWidth(), y2 - y1 ) );
+					return true;
+				}
+			}
+			else if ( _area.GetSize().GetHeight() == slot._area.GetSize().GetHeight() && _area.GetPosition().GetY() == slot._area.GetPosition().GetY() )
+			{
+				if( ( slot._area.GetPosition().GetX() >= _area.GetPosition().GetX() && slot._area.GetPosition().GetX() <= _area.GetLimit().GetX() ) ||
+					( slot._area.GetLimit().GetX() >= _area.GetPosition().GetX() && slot._area.GetLimit().GetX() <= _area.GetLimit().GetX() ) )
+				{
+					int x1 = std::min( slot._area.GetPosition().GetX(), _area.GetPosition().GetX() );
+					int x2 = std::max( slot._area.GetLimit().GetX(), _area.GetLimit().GetX() );
+
+					_area = Rectangle( Point( x1, slot._area.GetPosition().GetY() ), Size( x2 - x1, _area.GetSize().GetHeight() ) );
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+
+		bool Sheet::Place(const Item& item)
+		{
+			Placement placement( item );
+
+			double distance = 0;
+			bool placed = false;
+			for( std::vector<Slot>::iterator it = _slots.begin(); it != _slots.end(); ++it )
+			{
+				if( it->GetArea().GetSize().Fits( item.GetSize() ) )
+				{
+					double d = std::sqrt( std::pow( it->GetArea().GetPosition().GetX(), 2 ) + std::pow( it->GetArea().GetPosition().GetY(), 2 ) );
+					if( !placed || d < distance
+						/* it->GetArea().GetPosition().GetX() <= placement.GetArea().GetPosition().GetX() &&
+						   it->GetArea().GetPosition().GetY() <= placement.GetArea().GetPosition().GetY() */ )
+					{
+						placement.SetArea( it->GetArea().GetPosition(), item.GetSize() );
+
+						distance = d;
+						placed = true;
+					}
+				}
+			}
+
+			if( placed )
+			{
+				_placements.push_back( placement );
+
+				std::vector<Slot> slots;
+				for( std::vector<Slot>::iterator it = _slots.begin(); it != _slots.end(); ++it )
+					it->Place( placement, slots );
+
+				_slots = slots;
+			}
+
+			return placed;
+		}
 
 		Chromosome::GaChromosomePtr CspInitializator::operator ()(bool empty,
 			const Chromosome::GaInitializatorParams& parameters,
--- Problems/CSP.h
+++ Problems/CSP.h
@@ -11,6 +11,256 @@ namespace Problems
 	namespace BPP
 	{
 
+		class Size;
+
+		class Point
+		{
+
+		private:
+
+			int _x;
+
+			int _y;
+
+		public:
+
+			Point(int x, int y) : _x(x),
+				_y(y) { }
+
+			Point(const Point& src) : _x(src._x),
+				_y(src._y) { }
+
+			Point() : _x(0),
+				_y(0) { }
+
+			inline void SetX(int x) { _x = x; }
+
+			inline int GetX() const { return _x; }
+
+			inline void SetY(int y) { _y = y; }
+
+			inline int GetY() const { return _y; }
+
+			Point& operator +=(const Size& rhs);
+
+			Point& operator -=(const Size& rhs);
+
+			Point& operator =(const Point& rhs);
+
+			friend Point operator +(const Point& lhs, const Size& rhs);
+
+			friend Size operator -(const Point& lhs, const Point& rhs);
+
+			friend Point operator -(const Point& lhs, const Size& rhs);
+
+			friend bool operator ==(const Point& lhs, const Point& rhs);
+
+			friend bool operator !=(const Point& lhs, const Point& rhs);
+
+		};
+
+		class Size
+		{
+
+		private:
+
+			int _width;
+
+			int _height;
+
+		public:
+
+			Size(int width, int height) : _width(width),
+				_height(height) { }
+
+			Size(const Size& src) : _width(src._width),
+				_height(src._height) { }
+
+			Size() : _width(0),
+				_height(0) { }
+
+			inline void Rotate() { std::swap( _width, _height ); }
+
+			inline void SetWidth(int width) { _width = width; }
+
+			inline int GetWidth() const { return _width; }
+
+			inline void SetHeight(int height) { _height = height; }
+
+			inline int GetHeight() const { return _height; }
+
+			inline int GetArea() const { return _width * _height; }
+
+			inline Size GetRotated() const { return Size( _height, _width ); }
+
+			inline bool Fits(const Size& size) const { return _width >= size._width && _height >= size._height; }
+
+			Size& operator +=(const Size& rhs);
+
+			Size& operator -=(const Size& rhs);
+
+			Size& operator =(const Size& rhs);
+
+			friend Size operator +(const Size& lhs, const Size& rhs);
+
+			friend Point operator +(const Point& lhs, const Size& rhs);
+
+			friend Size operator -(const Size& lhs, const Size& rhs);
+
+			friend Point operator -(const Point& lhs, const Size& rhs);
+
+			friend bool operator ==(const Size& lhs, const Size& rhs);
+
+			friend bool operator !=(const Size& lhs, const Size& rhs);
+
+		};
+
+		inline Point operator +(const Point& lhs, const Size& rhs) { return Point( lhs._x + rhs._width, lhs._y + rhs._height ); }
+
+		inline Size operator -(const Point& lhs, const Point& rhs) { return Size( lhs._x - rhs._x, lhs._y - rhs._y ); }
+
+		inline Point operator -(const Point& lhs, const Size& rhs) { return Point( lhs._x - rhs._width, lhs._y - rhs._height ); }
+
+		inline bool operator ==(const Point& lhs, const Point& rhs) { return lhs._x == rhs._x && lhs._y == rhs._y; }
+
+		inline bool operator !=(const Point& lhs, const Point& rhs) { return lhs._x != rhs._x || lhs._y != rhs._y; }
+
+		inline Size operator +(const Size& lhs, const Size& rhs) { return Size( lhs._width + rhs._width, lhs._height + rhs._height ); }
+
+		inline Size operator -(const Size& lhs, const Size& rhs) { return Size( lhs._width - rhs._width, lhs._height - rhs._height ); }
+
+		inline bool operator ==(const Size& lhs, const Size& rhs) { return lhs._width == rhs._width && lhs._height == rhs._height; }
+
+		inline bool operator !=(const Size& lhs, const Size& rhs) { return lhs._width != rhs._width || lhs._height != rhs._height; }
+
+		class Rectangle
+		{
+
+		private:
+
+			Point _position;
+
+			Size _size;
+
+			Point _limit;
+
+		public:
+
+			Rectangle() { }
+
+			Rectangle(const Point& position, const Size& size) : _position(position),
+				_size(size),
+				_limit(_position + _size)  { }
+
+			inline void GetPosition(const Point& position) { _limit = ( _position = position ) + _size; }
+
+			inline const Point& GetPosition() const { return _position; }
+
+			inline void SetSize(const Size& size) { _limit = _position + ( _size = size ); }
+
+			inline const Size& GetSize() const { return _size; }
+
+			inline const Point& GetLimit() const { return _limit; }
+
+			inline bool IsOverlapping(const Rectangle& r)
+				{ return _position.GetX() < r._limit.GetX() && _limit.GetX() > r._position.GetX() && _position.GetY() < r._limit.GetY() && _limit.GetY() > r._position.GetY(); }
+
+		};
+
+		class Item
+		{
+
+		protected:
+
+			Size _size;
+
+			std::string _label;
+
+		public:
+
+			Item(const Size& size, const std::string& label) : _size(size),
+				_label(label) { }
+
+			Item() { }
+
+			inline const Size& GetSize() const { return _size; }
+
+			inline void SetSize(const Size& size) { _size = size; }
+
+			inline const std::string& GetLabel() const { return _label; }
+
+			inline void SetLabel(const std::string& label) { _label = label; }
+
+		};
+
+		class Placement
+		{
+
+		private:
+
+			const Item& _item;
+
+			Rectangle _area;
+
+		public:
+
+			Placement(const Item& item) : _item(item) { }
+
+			Placement(const Item& item, const Point& position, const Size& rotation) : _item(item),
+				_area(position, rotation) { }
+
+			inline const Item& GetItem() const { return _item; }
+
+			void SetArea(const Point& position, const Size& rotation) { _area = Rectangle( position, rotation ); }
+
+			inline const Rectangle& GetArea() const { return _area; }
+
+		};
+
+		class Slot
+		{
+
+		private:
+
+			Rectangle _area;
+
+		public:
+
+			Slot(const Point& position, const Size& size) : _area(position, size) { }
+
+			inline const Rectangle& GetArea() const { return _area; }
+
+			void Place(const Placement& placement, std::vector<Slot>& slots);
+
+			bool Merge(const Slot& slot);
+
+		};
+
+		class Sheet
+		{
+
+		private:
+
+			Size _size;
+
+			std::vector<Placement> _placements;
+
+			std::vector<Slot> _slots;
+
+		public:
+
+			Sheet(const Size& size) : _size(size) { _slots.push_back( Slot( Point(), _size ) ); }
+
+			bool Place(const Item& item);
+
+			inline const Size& GetSize() const { return _size; }
+
+			inline const std::vector<Placement> GetPlacements() const { return _placements; }
+
+			inline const std::vector<Slot> GetSlot() const { return _slots; }
+
+		};
+
 		class CspConfigBlock : public Chromosome::GaChromosomeConfigBlock
 		{
 
